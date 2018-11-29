90c17ba5970a2da299023abb077d1737c708aae7
Rohin <rohin_adalja@hotmail.com>
2018-11-29 15:12:11 -0500

    Updated test functions

--- projects/adaljar/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/adaljar/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -22,7 +22,8 @@ public class UrlValidatorTest extends TestCase {
       super(testName);
    }
 
-   
+
+
    
    public void testManualTest()
    {
@@ -54,7 +55,7 @@ public class UrlValidatorTest extends TestCase {
    {
 	   //You can use this function to implement your First Partition testing	  
       String[] validSchemesList = {"http://","https://","","ftp://"}; 
-      String[] invalidSchemesList = {"http:/","3ht://","http/", "http:", "://"}; 
+      String[] invalidSchemesList = {"http:/","3ht://","http/", "http:", "://","///:"}; 
       UrlValidator urlVal = new UrlValidator();
 
       //TEST SCHEMES: VALID (TRY URLS CONTAINING THE VALID SCHEMES)
@@ -77,11 +78,11 @@ public class UrlValidatorTest extends TestCase {
    
    public void testYourSecondPartition(){
 		//You can use this function to implement your Second Partition testing	   
-      String[] validAuthorityList = {"www.amazon.com","amazon.com","amazon.org","amazon.edu"};
-      String[] invalidAuthorityList = {"www.gmail%.com","ccc.gmail.com",""," ","www.gmail"};
+      String[] validAuthorityList = {"www.gmail.com","gmail.com","192.168.1.1"};
+      String[] invalidAuthorityList = {"www.gmail%.com","ccc.gmail.com",""," ","badtest","gmail","www.gmail"};
       UrlValidator urlVal = new UrlValidator();
 
-      //TEST AUTHORITY PARTITIONING: VALOD (TRY URLS CONTAINING THE VALID TEST AUTHORITY)
+      //TEST AUTHORITY PARTITIONING: VALID (TRY URLS CONTAINING THE VALID TEST AUTHORITY)
       System.out.println("\n\nTESTS: Running Second Partition Testing (Valid Authority)");
       for (int i = 0; i < validAuthorityList.length; i++) {
          String testURL = validScheme + validAuthorityList[i] + validPort + validPath;
@@ -101,21 +102,84 @@ public class UrlValidatorTest extends TestCase {
 
    //You need to create more test cases for your Partitions if you need to 
    public void testYourThirdPartition(){
-       //You can use this function to implement your Second Partition testing    
+      String[] validPortList = {"",":80", ":8080", ":433", ":6789", ":265", ":935", ":1115", ":5858",":15455", ":65530"};
+      String[] invalidPortList = {" ","aa",":-4", ":test32", ":", ":242342234"};
+      UrlValidator urlVal = new UrlValidator();
 
+      //TEST PORT PARTITIONING: VALID (TRY URLS CONTAINING THE VALID TEST PORTS)
+      System.out.println("\n\nTESTS: Running Third Partition Testing (Valid Port)");
+      for (int i = 0; i < validPortList.length; i++) {
+         String testURL = validScheme + validAuthority + validPortList[i] + validPath;
+         System.out.println("TESTING: " + testURL);
+         System.out.println(urlVal.isValid(testURL));
+      }
+
+      //TEST PORT PARTITIONING: INVALID (TRY URLS CONTAINING THE INVALID TEST PORTS)
+      System.out.println("\n\nTESTS: Running Third Partition Testing (Invalid Port)");
+      for (int i = 0; i < invalidPortList.length; i++) {
+         String testURL = validScheme + validAuthority + invalidPortList[i] + validPath;
+         System.out.println("TESTING: " + testURL);
+         System.out.println(urlVal.isValid(testURL));
+      }
+      System.out.println("\n\n***********************\n\n");
    }
 
-   public void testYourFourthPartition(){
-       //You can use this function to implement your Second Partition testing    
+   public void testYourFourthPartition(){  
+      String[] validPathList = {"","/","/path","/path/","/foo/bar" "/foo/bar/", "/1/2/3/4/" };
+      String[] invalidPathList = {"/path/...test","badpath","////alsobadpath", "//oopspath"};
+      UrlValidator urlVal = new UrlValidator();
 
+      //TEST QUERY PARTITIONING: VALID (TRY URLS CONTAINING THE VALID TEST PATHS)
+      System.out.println("\n\nTESTS: Running Fourth Partition Testing (Valid Path)");
+      for (int i = 0; i < validPathList.length; i++) {
+         String testURL = validScheme + validAuthority + validPort + validPathList[i];
+         System.out.println("TESTING: " + testURL);
+         System.out.println(urlVal.isValid(testURL));
+      }
+
+      //TEST QUERY PARTITIONING: INVALID (TRY URLS CONTAINING THE INVALID TEST PATHS)
+      System.out.println("\n\nTESTS: Running Fourth Partition Testing (Invalid Path)");
+      for (int i = 0; i < invalidPathList.length; i++) {
+         String testURL = validScheme + validAuthority + validPort + invalidPathList[i];
+         System.out.println("TESTING: " + testURL);
+         System.out.println(urlVal.isValid(testURL));
+      }
+      System.out.println("\n\n***********************\n\n");
    }
+
+   public void testYourFifthPartition(){  
+      String[] validQueryList = {"","?foo=bar","?state=oregon&best=true", "?foo+bar=foobar"};
+      String[] invalidQueryList = {"/ ?s","?a=b=c","?d e f","???foo=bar"};
+      UrlValidator urlVal = new UrlValidator();
+
+      //TEST QUERY PARTITIONING: VALID (TRY URLS CONTAINING THE VALID TEST QUERIES)
+      System.out.println("\n\nTESTS: Running Fifth Partition Testing (Valid Query)");
+      for (int i = 0; i < validQueryList.length; i++) {
+         String testURL = validScheme + validAuthority + validPort + validPath + validQueryList[i];
+         System.out.println("TESTING: " + testURL);
+         System.out.println(urlVal.isValid(testURL));
+      }
+
+      //TEST QUERY PARTITIONING: INVALID (TRY URLS CONTAINING THE INVALID TEST QUERIES)
+      System.out.println("\n\nTESTS: Running Fifth Partition Testing (Invalid query)");
+      for (int i = 0; i < invalidQueryList.length; i++) {
+         String testURL = validScheme + validAuthority + validPort + validPath + invalidQueryList[i];
+         System.out.println("TESTING: " + testURL);
+         System.out.println(urlVal.isValid(testURL));
+      }
+      System.out.println("\n\n***********************\n\n");
+   }
+
+
 
    public void testIsValid()
    {
-	   //You can use this function for programming based testing
+	  //You can use this function for programming based testing
 
    }
    
+
+
 
 
 }
